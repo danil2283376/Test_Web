b7d362fb76a635b327499e8185eaf8b229910d66
Danil <danil.vasiliev98@bk.ru>
2024-10-09 15:17:14 +0300

    Fixed Optimization Button script
    Fixed Optimization Button script

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,42 +5,33 @@ using UnityEngine.EventSystems;
 
 public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler 
 {
-    private bool _on;
-    private Animator animator;
+    public LoadSprites LoadSpritesParent;
+    public Animator Animator;
 
     public void OnPointerClick(PointerEventData eventData) 
     {
         Debug.Log("Click");
-        _on = true;
+        Animator.SetBool("anim", true);
+        LoadSpritesParent.load();
     }
 
     public void OnPointerDown(PointerEventData eventData) 
     {
-        throw new System.NotImplementedException();
+
     }
 
     public void OnPointerEnter(PointerEventData eventData) 
     {
-        throw new System.NotImplementedException();
+
     }
 
     public void OnPointerExit(PointerEventData eventData) 
     {
-        throw new System.NotImplementedException();
+
     }
 
     public void OnPointerUp(PointerEventData eventData) 
     {
-        throw new System.NotImplementedException();
-    }
 
-    void Update() 
-    {
-        if (_on) 
-        {
-            animator = GetComponent<Animator>();
-            animator.SetBool("anim", true);
-            GameObject.Find("Panel").GetComponent<LoadSprites>().load();
-        }
     }
 }
--- Assets/Scripts/LoadSprites.cs
+++ Assets/Scripts/LoadSprites.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
-public class LoadSprites : MonoBehaviour {
+public class LoadSprites : MonoBehaviour 
+{
     public GameObject[] spriteRenderer;
     public string[] strings;
 
-
-    public void load() {
-        for (int i = 0; i < spriteRenderer.Length; i++) {
+    public void load() 
+    {
+        for (int i = 0; i < spriteRenderer.Length; i++) 
+        {
             StartCoroutine(LoadSprite(spriteRenderer[i].GetComponent<SpriteRenderer>(), i));
         }
     }
 
-    IEnumerator LoadSprite(SpriteRenderer spriteRenderer, int number) {
+    IEnumerator LoadSprite(SpriteRenderer spriteRenderer, int number) 
+    {
         var task = Addressables.LoadAssetAsync<Sprite>(strings[number]);
         yield return task;
         spriteRenderer.sprite = task.Result;
     }
-
 }
