7c3761b1ebaddf8b56519704818b52bc31643d65
Danil <danil.vasiliev98@bk.ru>
2024-10-09 15:02:48 +0300

    Add EventSystem for working button)

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,32 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler {
-    bool on;
-    Animator animator;
-    public void OnPointerClick(PointerEventData eventData) {
+public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler 
+{
+    private bool _on;
+    private Animator animator;
+
+    public void OnPointerClick(PointerEventData eventData) 
+    {
         Debug.Log("Click");
-        on = true;
+        _on = true;
     }
 
-    public void OnPointerDown(PointerEventData eventData) {
+    public void OnPointerDown(PointerEventData eventData) 
+    {
         throw new System.NotImplementedException();
     }
 
-    public void OnPointerEnter(PointerEventData eventData) {
+    public void OnPointerEnter(PointerEventData eventData) 
+    {
         throw new System.NotImplementedException();
     }
 
-    public void OnPointerExit(PointerEventData eventData) {
+    public void OnPointerExit(PointerEventData eventData) 
+    {
         throw new System.NotImplementedException();
     }
 
-    public void OnPointerUp(PointerEventData eventData) {
+    public void OnPointerUp(PointerEventData eventData) 
+    {
         throw new System.NotImplementedException();
     }
 
-    void Update() {
-        if (on) {
+    void Update() 
+    {
+        if (_on) 
+        {
             animator = GetComponent<Animator>();
             animator.SetBool("anim", true);
             GameObject.Find("Panel").GetComponent<LoadSprites>().load();
