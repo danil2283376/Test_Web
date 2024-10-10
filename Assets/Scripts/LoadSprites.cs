ed707b035454d198d8e157663c71c38fe5396ff2
Danil <danil.vasiliev98@bk.ru>
2024-10-10 09:14:40 +0300

    Test Addressables in WebGL, on PC working!

--- Assets/Scripts/LoadSprites.cs
+++ Assets/Scripts/LoadSprites.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
-public class LoadSprites : MonoBehaviour 
+public class LoadSprites : MonoBehaviour
 {
     public SpriteRenderer[] SpriteRenderer;
+    //"Assets/Sprites/Image ";
+    //private readonly string _defaultPathToSprite = "Sprites/Image ";
     private readonly string _defaultPathToSprite = "Assets/Sprites/Image ";
 
     public void Load() 
@@ -15,9 +18,24 @@ public class LoadSprites : MonoBehaviour
         }
     }
 
-    private IEnumerator LoadSprite(SpriteRenderer spriteRenderer, int number) 
+    private IEnumerator LoadSprite(SpriteRenderer spriteRenderer, int number)
     {
+        //Debug.Log("For Test: " + _defaultPathToSprite + number/* + ".jpg"*/);
         var task = Addressables.LoadAssetAsync<Sprite>(_defaultPathToSprite + number + ".jpg");
+
+        //// Ожидаем завершения загрузки
+        //task.Completed += (operation) =>
+        //{
+        //    if (operation.Status == AsyncOperationStatus.Succeeded)
+        //    {
+        //        spriteRenderer.sprite = operation.Result; // Установка Sprite на SpriteRenderer
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("Ошибка загрузки: " + operation.Result.ToString()); //  operation.Result -  строковое описание ошибки
+        //    }
+        //};
+
         yield return task;
         spriteRenderer.sprite = task.Result;
     }
